7037cf6e1f05f44b41be02bef22c99b9e1b422da
Petar Georgiev <pkg_vt@abv.bg>
2015-11-10 21:15:42 +0200

    Модул 07, Видео 04, Помощ за всички команди

--- MP01/Calculations/Calculations.cs
+++ MP01/Calculations/Calculations.cs
@@ -9,10 +9,32 @@ namespace Calculations
 		public Formula02 F02 = new Formula02 ();
 		public Formula03 F03 = new Formula03 ();
 
+		//Библиотеки
+
+		private Colors.ForCLI _cl = new Colors.ForCLI ();
 
 		public MainLibrary ()
 		{
 		}
+
+		public void commands ()
+		{
+			_cl.Default (); Console.WriteLine("Налични команди\n");
+
+			_cl.Command (); Console.Write ("[яма]");
+			_cl.Default (); Console.WriteLine(" - пресмятане на строителна яма");
+
+			_cl.Command (); Console.Write ("[въгъл]");
+			_cl.Default (); Console.WriteLine(" - пресмятане на вдлъбнат ъгъл");
+
+			_cl.Command (); Console.Write ("[иъгъл]");
+			_cl.Default (); Console.WriteLine(" - пресмятане на изпъкнал ъгъл\n");
+
+		}
+
 	}
+
 }
+
+
 
--- MP01/UserInput/UserInput.cs
+++ MP01/UserInput/UserInput.cs
@@ -33,6 +33,21 @@ namespace UserInput
 				if (_command.ToLower().Contains("яма"))       _calc.F01.calc (_command);
 				if (_command.ToLower().Contains("въгъл"))     _calc.F02.calc (_command);
 				if (_command.ToLower().Contains("иъгъл"))     _calc.F03.calc (_command);
+
+				if (_command.ToLower().Contains("помощ") ||
+					_command.ToLower()== "п" )     			 _calc.commands ();
+
+				if (_command.ToLower()== "изчисти" ||
+					_command.ToLower()== "и" )
+
+				{
+					Console.Clear ();
+					_c.Default ();
+					Console.WriteLine ("Добре дошли в " + abaut.shortName + "\n"+ abaut.version + "\n" );
+
+				}
+
+								 
 			}
 			while (_command.ToLower() !="изход");
 		}
